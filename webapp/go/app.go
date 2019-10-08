cbf579e272a97f01a9135da5c23b9f5980f31ccd
stomita083 <s-tomita@unext.jp>
2019-10-08 20:23:06 +0900

    realize

--- webapp/go/app.go
+++ webapp/go/app.go
@@ -3,34 +3,22 @@ package main
 import (
 	"database/sql"
 	"log"
-	//"encoding/json"
-	// "net"
 	"net/http"
-	//"fmt"
-	//"os"
-	// "os/exec"
-	// "text/template"
 	"time"
-
-	// "golang.org/x/net/netutil"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	// "github.com/pkg/errors"
-	// "github.com/takashabe/go-router"
-	// "github.com/takashabe/go-session"
-	// _ "github.com/takashabe/go-session/memory"
 )
 
 // DB table mapping
 type User struct {
-	ID        int  `json:"id,omitempty"`
-	Name      string  `json:"name,omitempty"`
-	Email     string  `json:"email,omitempty"`
-	Passhash  string  `json:"passhash,omitempty"`
-	CreatedAt time.Time  `json:"created_at,omitempty"`
-	UpdatedAt time.Time  `json:"updated_at,omitempty"`
+	ID        int       `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Passhash  string    `json:"passhash,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }
 
 // DB table mapping
@@ -84,16 +72,10 @@ type Following struct {
 var db *sql.DB
 
 func main() {
-	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4",
-	// 	os.Getenv("DB_USER"), os.Getenv("DB_PASS"),
-	// 	"localhost", os.Getenv("DB_PORT"),
-	// 	os.Getenv("DB_DATABASE"),
-	// )
 
 	var err error
-	//db, err = sql.Open("mysql", dsn)
-	//db, err := sql.Open("mysql", "isucon:isucon@/isucon?parseTime=true&charset=utf8mb4")
-	db, err := sql.Open("mysql", "isucon:isucon@unix(/tmp/mysql.sock)/isucon?parseTime=true&charset=utf8mb4")
+	db, err := sql.Open("mysql", "app:app@/app?parseTime=true&charset=utf8mb4")
+	//db, err := sql.Open("mysql", "app:app@unix(/tmp/mysql.sock)/isucon?parseTime=true&charset=utf8mb4")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -106,109 +88,109 @@ func main() {
 	// 	},
 	// }
 
-	    // 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
-		e.Use(middleware.Logger())
-		e.Use(middleware.Recover())
-	
-		// ルーティング
-		e.GET("/hello", func(c echo.Context) error {
-			return c.String(http.StatusOK, "Hello World")
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
+
+	e.GET("/users", func(c echo.Context) error {
+		tx, err := db.Begin()
+		if err != nil {
+			return err
+		}
+		defer tx.Commit()
+
+		var user User
+		if err := tx.QueryRow("SELECT * FROM user").Scan(&user.ID, &user.Name, &user.Passhash, &user.CreatedAt, &user.UpdatedAt); err != sql.ErrNoRows {
+			//tx.Rollback()
+			if err == nil {
+				//return resError(c, "duplicated", 409)
+				return err
+			}
+			return err
+		}
+
+		return c.JSON(200, echo.Map{
+			"id":   user.ID,
+			"name": user.Name,
+		})
+	})
+
+	e.GET("/users/:id", func(c echo.Context) error {
+		tx, err := db.Begin()
+		if err != nil {
+			return err
+		}
+		defer tx.Commit()
+
+		id := c.Param("id")
+		var user User
+		if err := tx.QueryRow("SELECT * FROM user WHERE id = ?", id).Scan(&user.ID, &user.Name, &user.Passhash, &user.CreatedAt, &user.UpdatedAt); err != sql.ErrNoRows {
+			//tx.Rollback()
+			if err == nil {
+				//return resError(c, "duplicated", 409)
+				return err
+			}
+			return err
+		}
+
+		return c.JSON(200, echo.Map{
+			"id":   user.ID,
+			"name": user.Name,
+		})
+	})
+
+	e.POST("/users/:id/tweet", func(c echo.Context) error {
+		var params struct {
+			Content string `json:"content"`
+		}
+		c.Bind(&params)
+
+		// require login
+		// user, err := getCurrentUser(w, r)
+		// if err != nil {
+		// 	http.Redirect(w, r, "/login", 302)
+		// 	return
+		// }
+
+		id := c.Param("id")
+
+		tx, err := db.Begin()
+		if err != nil {
+			return err
+		}
+
+		res, err := db.Exec("INSERT INTO tweet (user_id, content) VALUES (?,?)", id, params.Content)
+		if err != nil {
+			tx.Rollback()
+			return resError(c, "", 0)
+		}
+		userID, err := res.LastInsertId()
+		if err != nil {
+			tx.Rollback()
+			return resError(c, "", 0)
+		}
+		if err := tx.Commit(); err != nil {
+			return err
+		}
+
+		return c.JSON(201, echo.Map{
+			"id":      userID,
+			"content": params.Content,
 		})
 
-		e.GET("/users", func(c echo.Context) error {
-			tx, err := db.Begin()
-			if err != nil {
-				return err
-			}
-			defer tx.Commit()
-
-			var user User
-			if err := tx.QueryRow("SELECT * FROM user").Scan(&user.ID, &user.Name, &user.Passhash, &user.CreatedAt, &user.UpdatedAt); err != sql.ErrNoRows {
-				//tx.Rollback()
-				if err == nil {
-					//return resError(c, "duplicated", 409)
-					return err
-				}
-				return err
-			}
-
-			return c.JSON(200, echo.Map{
-				"id":       user.ID,
-				"name": user.Name,
-			})
-		})
-
-		e.GET("/users/:id", func(c echo.Context) error {
-			tx, err := db.Begin()
-			if err != nil {
-				return err
-			}
-			defer tx.Commit()
-
-			id := c.Param("id")
-			var user User
-			if err := tx.QueryRow("SELECT * FROM user WHERE id = ?", id).Scan(&user.ID, &user.Name, &user.Passhash, &user.CreatedAt, &user.UpdatedAt); err != sql.ErrNoRows {
-				//tx.Rollback()
-				if err == nil {
-					//return resError(c, "duplicated", 409)
-					return err
-				}
-				return err
-			}
-
-			return c.JSON(200, echo.Map{
-				"id":       user.ID,
-				"name": user.Name,
-			})
-		})
-
-		e.POST("/users/:id/tweet", func(c echo.Context) error {
-			var params struct {
-				Content  string `json:"content"`
-			}
-			c.Bind(&params)
-
-			// require login
-			// user, err := getCurrentUser(w, r)
-			// if err != nil {
-			// 	http.Redirect(w, r, "/login", 302)
-			// 	return
-			// }
-
-			id := c.Param("id")
-
-			tx, err := db.Begin()
-			if err != nil {
-				return err
-			}
-
-			res, err := db.Exec("INSERT INTO tweet (user_id, content) VALUES (?,?)", id, params.Content)
-			if err != nil {
-				tx.Rollback()
-				return resError(c, "", 0)
-			}
-			userID, err := res.LastInsertId()
-			if err != nil {
-				tx.Rollback()
-				return resError(c, "", 0)
-			}
-			if err := tx.Commit(); err != nil {
-				return err
-			}
-	
-			return c.JSON(201, echo.Map{
-				"id":       userID,
-				"content": params.Content,
-			})
-
-			//http.Redirect(w, r, "/", 302)
+		//http.Redirect(w, r, "/", 302)
 	})
 
 	// サーバー起動
 	e.Start(":8080")
 }
 
-	func resError(c echo.Context, e string, status int) error {
+func resError(c echo.Context, e string, status int) error {
 	if e == "" {
 		e = "unknown"
 	}
---
+++ webapp/go/go.mod
@@ -0,0 +1,12 @@
+module github.com/sora083/go-rest/webapp/go
+
+go 1.12
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc // indirect
+)
